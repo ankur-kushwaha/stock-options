4eea296c8c5802b5a14a5e77b410cf2b03fcdf47
Ankur Kushwaha <ankushwaha@expedia.com>
2021-08-27 19:52:53 +0530

    fix

--- src/pages/test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import useTicker from '../helpers/useTicker';
-
-export default function Test(){
-  let {ticks}  = useTicker([31512322])
-  
-  return <h1>Socket.io </h1>
-}
