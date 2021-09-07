1b2d0d2f5ced2e1c071668fae03505c91690b6dc
Ankur Kushwaha <ankushwaha@expedia.com>
2021-09-07 10:13:53 +0530

    title updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Smart Options</title>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
           
         </Head>
